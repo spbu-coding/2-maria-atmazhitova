ccf333214239a8f761f5efcdcb9c5ac570cfa8d9
maria-atmazhitova <ertemilly@gmail.com>
2020-10-16 01:49:17 +0300

    Attemt to fix .s file

--- src/sort.s
+++ src/sort.s
@@ -1,106 +1,125 @@
 	.file	"sort.c"
 	.text
 	.globl	sort
-	.def	sort;	.scl	2;	.type	32;	.endef
-	.seh_proc	sort
+	.type	sort, @function
 sort:
+.LFB0:
+	.cfi_startproc
+	endbr64
 	pushq	%rbp
-	.seh_pushreg	%rbp
+	.cfi_def_cfa_offset 16
+	.cfi_offset 6, -16
 	movq	%rsp, %rbp
-	.seh_setframe	%rbp, 0
-	subq	$48, %rsp
-	.seh_stackalloc	48
-	.seh_endprologue
-	movq	%rcx, 16(%rbp)
-	movq	%rdx, 24(%rbp)
-	movq	%r8, 32(%rbp)
-	movq	$0, -8(%rbp)
+	.cfi_def_cfa_register 6
+	movq	%rdi, -56(%rbp)
+	movq	%rsi, -64(%rbp)
+	movq	%rdx, -72(%rbp)
+	movq	$0, -32(%rbp)
 	jmp	.L2
 .L3:
-	movq	-8(%rbp), %rax
+	movq	-32(%rbp), %rax
 	leaq	0(,%rax,8), %rdx
-	movq	16(%rbp), %rax
+	movq	-56(%rbp), %rax
 	addq	%rdx, %rax
-	movq	-8(%rbp), %rdx
+	movq	-32(%rbp), %rdx
 	leaq	0(,%rdx,8), %rcx
-	movq	24(%rbp), %rdx
+	movq	-64(%rbp), %rdx
 	addq	%rcx, %rdx
 	movq	(%rax), %rax
 	movq	%rax, (%rdx)
-	addq	$1, -8(%rbp)
+	addq	$1, -32(%rbp)
 .L2:
-	movq	-8(%rbp), %rax
-	cmpq	32(%rbp), %rax
+	movq	-32(%rbp), %rax
+	cmpq	-72(%rbp), %rax
 	jb	.L3
-	movq	$0, -16(%rbp)
+	movq	$0, -24(%rbp)
 	jmp	.L4
 .L9:
-	movq	-16(%rbp), %rax
-	movq	%rax, -24(%rbp)
-	movq	-16(%rbp), %rax
+	movq	-24(%rbp), %rax
+	movq	%rax, -16(%rbp)
+	movq	-24(%rbp), %rax
 	addl	$1, %eax
-	movl	%eax, -28(%rbp)
+	movl	%eax, -36(%rbp)
 	jmp	.L5
 .L7:
-	movl	-28(%rbp), %eax
+	movl	-36(%rbp), %eax
 	cltq
 	leaq	0(,%rax,8), %rdx
-	movq	24(%rbp), %rax
+	movq	-64(%rbp), %rax
 	addq	%rdx, %rax
 	movq	(%rax), %rdx
-	movq	-24(%rbp), %rax
+	movq	-16(%rbp), %rax
 	leaq	0(,%rax,8), %rcx
-	movq	24(%rbp), %rax
+	movq	-64(%rbp), %rax
 	addq	%rcx, %rax
 	movq	(%rax), %rax
 	cmpq	%rax, %rdx
 	jge	.L6
-	movl	-28(%rbp), %eax
+	movl	-36(%rbp), %eax
 	cltq
-	movq	%rax, -24(%rbp)
+	movq	%rax, -16(%rbp)
 .L6:
-	addl	$1, -28(%rbp)
+	addl	$1, -36(%rbp)
 .L5:
-	movl	-28(%rbp), %eax
+	movl	-36(%rbp), %eax
 	cltq
-	cmpq	%rax, 32(%rbp)
+	cmpq	%rax, -72(%rbp)
 	ja	.L7
-	movq	-24(%rbp), %rax
-	cmpq	-16(%rbp), %rax
-	je	.L8
 	movq	-16(%rbp), %rax
+	cmpq	-24(%rbp), %rax
+	je	.L8
+	movq	-24(%rbp), %rax
 	leaq	0(,%rax,8), %rdx
-	movq	24(%rbp), %rax
+	movq	-64(%rbp), %rax
 	addq	%rdx, %rax
 	movq	(%rax), %rax
-	movq	%rax, -40(%rbp)
-	movq	-24(%rbp), %rax
+	movq	%rax, -8(%rbp)
+	movq	-16(%rbp), %rax
 	leaq	0(,%rax,8), %rdx
-	movq	24(%rbp), %rax
+	movq	-64(%rbp), %rax
 	addq	%rdx, %rax
-	movq	-16(%rbp), %rdx
+	movq	-24(%rbp), %rdx
 	leaq	0(,%rdx,8), %rcx
-	movq	24(%rbp), %rdx
+	movq	-64(%rbp), %rdx
 	addq	%rcx, %rdx
 	movq	(%rax), %rax
 	movq	%rax, (%rdx)
-	movq	-24(%rbp), %rax
+	movq	-16(%rbp), %rax
 	leaq	0(,%rax,8), %rdx
-	movq	24(%rbp), %rax
+	movq	-64(%rbp), %rax
 	addq	%rax, %rdx
-	movq	-40(%rbp), %rax
+	movq	-8(%rbp), %rax
 	movq	%rax, (%rdx)
 .L8:
-	addq	$1, -16(%rbp)
+	addq	$1, -24(%rbp)
 .L4:
-	movq	32(%rbp), %rax
+	movq	-72(%rbp), %rax
 	subq	$1, %rax
-	cmpq	%rax, -16(%rbp)
+	cmpq	%rax, -24(%rbp)
 	jb	.L9
 	nop
 	nop
-	addq	$48, %rsp
 	popq	%rbp
+	.cfi_def_cfa 7, 8
 	ret
-	.seh_endproc
-	.ident	"GCC: (Rev3, Built by MSYS2 project) 10.2.0"
+	.cfi_endproc
+.LFE0:
+	.size	sort, .-sort
+	.ident	"GCC: (Ubuntu 9.3.0-17ubuntu1~20.04) 9.3.0"
+	.section	.note.GNU-stack,"",@progbits
+	.section	.note.gnu.property,"a"
+	.align 8
+	.long	 1f - 0f
+	.long	 4f - 1f
+	.long	 5
+0:
+	.string	 "GNU"
+1:
+	.align 8
+	.long	 0xc0000002
+	.long	 3f - 2f
+2:
+	.long	 0x3
+3:
+	.align 8
+4:
