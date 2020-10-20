f100975c0d75b7571b402dd700245da0548f5be1
maria-atmazhitova <ertemilly@gmail.com>
2020-10-20 20:14:32 +0300

    Fixed sort function

--- src/sort.s
+++ src/sort.s
@@ -7,38 +7,40 @@ sort:
 .LFB16:
 	.cfi_startproc
 	endbr64
-	movq	%rsi, %r9
-	subq	$1, %r9
+	testq	%rsi, %rsi
 	je	.L1
-	xorl	%ecx, %ecx
+	xorl	%r8d, %r8d
+	leaq	1(%r8), %rcx
+	cmpq	%rcx, %rsi
+	jbe	.L1
 	.p2align 4,,10
 	.p2align 3
-.L6:
-	movq	%rcx, %r8
-	addq	$1, %rcx
-	cmpq	%rcx, %rsi
-	jbe	.L3
+.L15:
 	movq	%rcx, %rax
 	movq	%r8, %rdx
 	.p2align 4,,10
 	.p2align 3
-.L5:
-	movq	(%rdi,%rdx,8), %r10
-	cmpq	%r10, (%rdi,%rax,8)
+.L4:
+	movq	(%rdi,%rdx,8), %r9
+	cmpq	%r9, (%rdi,%rax,8)
 	cmovl	%rax, %rdx
 	addq	$1, %rax
 	cmpq	%rax, %rsi
-	jne	.L5
+	jne	.L4
 	cmpq	%r8, %rdx
-	je	.L3
+	je	.L8
 	leaq	(%rdi,%rdx,8), %rax
 	movq	-8(%rdi,%rcx,8), %r8
 	movq	(%rax), %rdx
 	movq	%rdx, -8(%rdi,%rcx,8)
 	movq	%r8, (%rax)
-.L3:
-	cmpq	%r9, %rcx
-	jne	.L6
+	cmpq	%rcx, %rsi
+	jbe	.L1
+.L8:
+	movq	%rcx, %r8
+	leaq	1(%r8), %rcx
+	cmpq	%rcx, %rsi
+	ja	.L15
 .L1:
 	ret
 	.cfi_endproc
